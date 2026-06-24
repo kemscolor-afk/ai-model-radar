447fcd8a9b7099701604b3acf8605ab28cf04fa6
kemscolor-afk <kemscolor-afk@users.noreply.github.com>
2026-06-24 23:29:49 +0800

    chore: support process.env.PORT for deployment

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ import type {
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // ============================================================
